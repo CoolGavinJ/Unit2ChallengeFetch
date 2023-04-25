c62f4d828382d147ccbdaa2354655dd91efd183d
Gavin J <coolgavinjay@gmail.com>
2023-04-25 14:07:45 -0400

    Finished Step 7

--- Unit2ChallengeFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Unit2ChallengeFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
+    public int ballIndex;
 
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
@@ -25,8 +26,12 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        
     }
 
 }
